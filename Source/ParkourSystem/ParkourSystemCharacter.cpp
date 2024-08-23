0c0f8265d77443076cb8e0110ae6cd670e1956b5
Oleksandr Smirnov <116755622+oophiuchuss@users.noreply.github.com>
2024-08-23 19:37:57 +0200

    Minor fixes.

--- Source/ParkourSystem/ParkourSystemCharacter.cpp
+++ Source/ParkourSystem/ParkourSystemCharacter.cpp
@@ -40,7 +40,7 @@ AParkourSystemCharacter::AParkourSystemCharacter()
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
 
-	// Create a camera boom (pulls in towards the player if there is a collision)
+	// Create a camera boom (pulls in towards the character if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
